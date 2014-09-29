150b68384d74181d837a8139425a03c8c4f9b776
ak <ak@laminar.ru>
2014-09-29 13:23:19 +0600

    Теперь запускается под XP

--- targetver.h
+++ targetver.h
@@ -11,8 +11,8 @@
 #define WINVER 0x0600           // Change this to the appropriate value to target other versions of Windows.
 #endif
 
-#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows Vista.
-#define _WIN32_WINNT 0x0600     // Change this to the appropriate value to target other versions of Windows.
+#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is XP.
+#define _WIN32_WINNT _WIN32_WINNT_WINXP     // Change this to the appropriate value to target other versions of Windows.
 #endif
 
 #ifndef _WIN32_WINDOWS          // Specifies that the minimum required platform is Windows 98.
@@ -20,5 +20,5 @@
 #endif
 
 #ifndef _WIN32_IE                       // Specifies that the minimum required platform is Internet Explorer 7.0.
-#define _WIN32_IE 0x0700        // Change this to the appropriate value to target other versions of IE.
+#define _WIN32_IE 0x0600        // Change this to the appropriate value to target other versions of IE.
 #endif
